c93a5db5277dd8abd5308772252273a01fa6e324
DenjiMk2 <13j5137@stu.meisei-u.ac.jp>
2018-06-05 21:06:05 +0900

    fate:connectElement関数を追加

--- src/js/init.js
+++ src/js/init.js
@@ -5,6 +5,13 @@ $(function(){
   for(var i=0;i<6;i++){
     $('#enemyTeam').append('<input type=text id="enemy'+i+'">');
   }
+
+  $('#friendTeam').on('input','input',function(){
+    if(/(friend|enemy)[1-6]/.test($(this).val())){
+      connectElement($(this).get(0),$('#'+$(this).val()).get(0));
+    }
+    console.log($(this).val());
+  });
 });
 var getFriendTeamName = function(){
   var ret = [];
@@ -19,4 +26,14 @@ var getEnemyTeamName = function(){
     ret.push($(this).val());
   });
   return ret;
+}
+var connectElement = function(from,to){
+  new LeaderLine(
+    from,
+    to,
+    {
+      dash: {animation: true},
+      color: 'rgba(30, 130, 250, 0.5)'
+    }
+  );
 }
